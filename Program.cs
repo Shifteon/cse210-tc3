399a4f031bf53e144b331b03063381c2006a33bb
Sasha Cheek <sasha@cheek.tv>
2021-09-29 14:19:13 -0600

    Adding my function to the cs file

--- Program.cs
+++ Program.cs
@@ -14,10 +14,16 @@ namespace cse210_tc3
             Console.WriteLine("Hello, I'm Michael!");
         }
 
+        static void DisplayGreetingSasha()
+        {
+            Console.WriteLine("Sasha says Hello");
+        }
+
         static void Main(string[] args)
         {
             DisplayGreetingBen();
             DisplayGreetingMichael();
+            DisplayGreetingSasha();
         }
     }
 }
